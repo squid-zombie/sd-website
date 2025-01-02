5737dc5e5a26c45a78bd5b0d8db9351de70cb3db
zombie-squid <maxl@mspgroupllc.com>
2025-01-01 20:43:53 -0800

    Updated style

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const button = document.getElementById("changeColor");
-  const panel = document.querySelector(".interactive");
-
-  // Array of bright modern colors
-  const colors = ["#FF6F61", "#6B5B95", "#88B04B", "#F7CAC9", "#92A8D1", "#955251"];
-
-  button.addEventListener("click", () => {
-    // Select a random color
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    panel.style.background = randomColor;
-  });
+  console.log("Welcome to Squidiablo's Modern Art Space!");
 });
